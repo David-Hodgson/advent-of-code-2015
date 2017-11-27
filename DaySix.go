6e0c3b095480002f93f044fc6075fdc4e5da8001
David-Hodgson <david.hodgson@feefo.com>
2017-11-27 15:34:32 +0000

    Day Size - Initial Work

---
+++ DaySix.go
@@ -0,0 +1,21 @@
+package adventofcode2015
+
+import (
+	"fmt"
+)
+
+func DaySixPartOne() {
+
+	const gridWidth = 1000
+	const gridHeight = 1000
+
+	fmt.Println("Day Six")
+
+	fmt.Println("Grid Width: ", gridWidth)
+	fmt.Println("Grid Height: ", gridHeight)
+
+	var grid [gridHeight][gridHeight]bool
+
+	fmt.Println("grid size: ", len(grid))
+
+}
