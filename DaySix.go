07f42f69b833029ab256f1269cbe9ffe362be552
David-Hodgson <david.hodgson@feefo.com>
2017-11-27 17:05:25 +0000

    Added method to change pixel values

--- DaySix.go
+++ DaySix.go
@@ -4,9 +4,23 @@ import (
 	"fmt"
 )
 
+func setPixel(grid [][]bool, x,y int, state bool) {
+	grid[y][x] = state
+}
+
+func togglePixel(grid [][]bool, x,y int) {
+	grid[y][x] = !grid[y][x]
+}
+
+func turnPixelOn(grid [][]bool, x,y int, state bool) {
+	setPixel(grid,x,y,true)
+}
+
+func turnPixelOff(grid [][]bool, x,y int, state bool) {
+	setPixel(grid,x,y,false)
+}
 
 func showGrid(grid [][]bool) {
-
 
 	for i :=0 ; i< len(grid); i++ {
 		for j :=0 ; j <len(grid[i]); j++ {
@@ -16,7 +30,6 @@ func showGrid(grid [][]bool) {
 			} else {
 				fmt.Print(" ")
 			}
-
 		}
 
 		fmt.Print("\n")
@@ -40,7 +53,9 @@ func DaySixPartOne() {
 	for i :=0 ; i< gridHeight ; i++ {
 		grid[i] = make([]bool, gridWidth)
 	}
-	
+
+	//grid[9][5] = true
+	setPixel(grid,5,9,true)
 	fmt.Println("grid size: ", len(grid))
 
 	showGrid(grid)
