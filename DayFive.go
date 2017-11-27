cdcedf9635906f4dc2aeed4cb038b2a3480f30df
David-Hodgson <david.hodgson@feefo.com>
2017-11-27 11:55:48 +0000

    Day Five - Part Two Complete

--- DayFive.go
+++ DayFive.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-func isSentenceNice(sentence string) bool {
+func isSentenceNiceV1(sentence string) bool {
 
 	nodisallowed := true
 	if strings.Contains(sentence,"ab") || strings.Contains(sentence,"cd") || strings.Contains(sentence,"pq") || strings.Contains(sentence,"xy") {
@@ -30,6 +30,36 @@ func isSentenceNice(sentence string) bool {
 	return threeVowels && doubleletters && nodisallowed
 }
 
+func isSentenceNiceV2(sentence string) bool {
+
+	letterPairs := false
+	repeats := false
+
+	for i := 0; i<len(sentence)-2;i++ {
+
+		if (sentence[i] == sentence[i+2]) {
+			repeats = true
+		}
+	}
+
+	pairs := make(map[string]int)
+	for i := 0; i<len(sentence)-1;i++ {
+
+		pair := sentence[i:i+2]
+
+		if pos,exists := pairs[pair]; exists {
+			if i > pos+1 {
+				letterPairs = true
+				break
+			}
+		} else {
+			pairs[pair] = i
+		}
+	}
+
+	return letterPairs && repeats
+}
+
 func DayFivePartOne() {
 
 	input := ReadFile("day5-input.txt")
@@ -40,10 +70,27 @@ func DayFivePartOne() {
 
 	for i := 0; i<len(sentences); i++ {
 
-		if isSentenceNice(sentences[i]) {
+		if isSentenceNiceV1(sentences[i]) {
 			niceCount++
 		}
+	}
 
+	fmt.Println("Nice Sentences: ",niceCount)
+}
+
+func DayFivePartTwo() {
+
+	input := ReadFile("day5-input.txt")
+
+	sentences := strings.Split(input, "\n")
+
+	niceCount := 0
+
+	for i := 0; i<len(sentences); i++ {
+
+		if isSentenceNiceV2(sentences[i]) {
+			niceCount++
+		}
 	}
 
 	fmt.Println("Nice Sentences: ",niceCount)
