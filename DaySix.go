7e2412239e49ee67d5c2e99e62f7c4e055d8d8b4
David-Hodgson <david.hodgson@feefo.com>
2017-11-27 16:21:29 +0000

    Initialise array correctly

--- DaySix.go
+++ DaySix.go
@@ -4,18 +4,44 @@ import (
 	"fmt"
 )
 
+
+func showGrid(grid [][]bool) {
+
+
+	for i :=0 ; i< len(grid); i++ {
+		for j :=0 ; j <len(grid[i]); j++ {
+
+			if grid[i][j] {
+				fmt.Print("#")
+			} else {
+				fmt.Print(" ")
+			}
+
+		}
+
+		fmt.Print("\n")
+	}
+}
+
 func DaySixPartOne() {
 
-	const gridWidth = 1000
-	const gridHeight = 1000
+	const gridWidth = 10
+	const gridHeight = 10
 
 	fmt.Println("Day Six")
 
 	fmt.Println("Grid Width: ", gridWidth)
 	fmt.Println("Grid Height: ", gridHeight)
 
-	var grid [gridHeight][gridHeight]bool
+	var grid [][]bool
 
+	grid = make([][]bool,gridHeight)
+
+	for i :=0 ; i< gridHeight ; i++ {
+		grid[i] = make([]bool, gridWidth)
+	}
+	
 	fmt.Println("grid size: ", len(grid))
 
+	showGrid(grid)
 }
