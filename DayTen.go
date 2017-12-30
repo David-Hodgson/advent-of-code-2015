dd4d5385ed40a748c43e3806b26b5d6551ce6a79
David Hodgson <dahodgson@gmail.com>
2017-12-30 22:13:50 +0000

    Day 10 - Part Two Complete using Cosmological Theorem

--- DayTen.go
+++ DayTen.go
@@ -63,6 +63,7 @@ type element struct {
 	number int
 	name string
 	pattern string
+	nextStage []string
 }
 
 
@@ -71,7 +72,6 @@ func DayTenPartTwo() {
 	fmt.Println("Day 10 - Part Two")
 
 	input := "1113122113"
-	//input = "13112221133211322112211213322112"
 
 	fmt.Println(input)
 	elementFile := strings.Split(ReadFile("day10-elements.txt"),"\n")
@@ -84,50 +84,81 @@ func DayTenPartTwo() {
 		elementNumber,_ := strconv.Atoi(elementParts[1])
 		elementName := elementParts[2]
 		elementPattern := elementParts[3]
+		var nextStage []string
 
-		elementList[elementNumber] = element{elementNumber,elementName,elementPattern}
+		elementList[elementNumber] = element{elementNumber,elementName,elementPattern,nextStage}
 	}
-
-	fmt.Println(elementList)
-
-	//TODO parse elementList to build next elements
-
 	//for each element
 		// look at next pattern
 		// if simple next is next elment
 		// else next is a list of elements including the next one
 
-	//TODO method to work through next states
-}
-
-func getNextState(input string, elementPatternMap, elementNameMap map[string]element, patternPosMap map[string]int, elementList []element) string {
-
-	currentElement := elementPatternMap[input]
-	currentPos := patternPosMap[input]
-
-	fmt.Println(currentElement)
-	fmt.Println(currentPos)
-
-	if currentPos >= len(elementList) {
-		fmt.Println("Last Element")
-	} else {
-		nextElement := elementList[currentPos+1]
-
-		nextElementGroups := strings.Split(nextElement.pattern, ".")
-
-		output := ""
-
-		for i := 0 ; i < len(nextElementGroups); i++ {
-			groupPattern := nextElementGroups[i]
-			output += "."
-			if otherElement, exists := elementNameMap[groupPattern]; exists {
-				output += otherElement.pattern
-			} else {
-				output += groupPattern
-			}
-
-		}
-		return output 
+	elementNameMap := buildNextStateMap(elementList)
+	var output []element
+	output = append(output, elementNameMap["Fr"])
+	for x :=0 ; x < 50 ; x++ {
+		output = getNextState(output, elementNameMap)
 	}
-	return input
+	finalPatternLength := 0
+
+	for y:= 0; y<len(output);y++ {
+		finalPatternLength += len(output[y].pattern)
+	}
+	fmt.Println(finalPatternLength)
 }
+
+func getNextState(input []element, elementMap map[string]element) []element {
+
+	output := make([]element,0)
+
+	for i := 0; i < len(input); i++ {
+		inputElement := input[i]
+		for j := 0; j < len(inputElement.nextStage); j++ {
+			nextElement := elementMap[inputElement.nextStage[j]]
+			output = append(output, nextElement)
+		}
+	}
+	return output
+
+}
+
+func buildNextStateMap(elementList []element) map[string]element {
+
+	elementNameMap := make(map[string]element)
+	elementNames := make(map[string]element)
+
+	for _,value := range elementList {
+		elementNames[value.name] = value
+	}
+
+	for i := 1; i < len(elementList) ; i++ {
+
+		currentElement := elementList[i]
+		//fix pattern
+		newPattern := ""
+		var nextStageArray []string
+		oldPatternGroups := strings.Split(currentElement.pattern,".")
+		for j :=0 ; j < len(oldPatternGroups) ; j++ {
+			if _,exists := elementNames[oldPatternGroups[j]];!exists {
+				newPattern = oldPatternGroups[j]
+				nextStageArray = append(nextStageArray,currentElement.name)
+			} else {
+				nextStageArray = append(nextStageArray, oldPatternGroups[j])
+			}
+		}
+
+		if i+1 < len(elementList) {
+			elementList[i+1].nextStage = nextStageArray
+		}
+		currentElement.pattern = newPattern
+		elementNameMap[currentElement.name] = currentElement
+	}
+
+	firstElement := elementNameMap["H"]
+	firstElement.nextStage = []string {"H"}
+	elementNameMap["H"] = firstElement
+
+	return elementNameMap
+}
+
+
