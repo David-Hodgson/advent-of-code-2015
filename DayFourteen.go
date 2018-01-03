0aee156403f8f0489218dd4d37e04e2aff9ac35c
David Hodgson <dahodgson@gmail.com>
2018-01-03 21:27:29 +0000

    Day 14 - Part One Complete

---
+++ DayFourteen.go
@@ -0,0 +1,92 @@
+package adventofcode2015
+
+import (
+	"fmt"
+	"strings"
+	"strconv"
+)
+
+type deerStats struct {
+	name string
+	speed, flyingTime, restingTime int
+}
+
+func parseReindeerStats(reindeerStats string) deerStats {
+
+	statParts := strings.Split(reindeerStats, " ")
+
+	name := statParts[0]
+	speed, _ := strconv.Atoi(statParts[3])
+	flyingTime, _ := strconv.Atoi(statParts[6])
+	restingTime, _ := strconv.Atoi(statParts[13])
+
+	return deerStats{name,speed,flyingTime,restingTime}
+}
+
+func getDistanceForTime(stats deerStats, time int) int {
+
+	distance := 0
+	cycleTime := stats.flyingTime + stats.restingTime
+
+	fullCycleCount := time / cycleTime
+	fullCycleDistance := (stats.speed * stats.flyingTime) * fullCycleCount
+
+	timeLeft := time - (cycleTime * fullCycleCount)
+
+	distance = fullCycleDistance
+
+	if timeLeft >= stats.flyingTime {
+		distance += (stats.speed * stats.flyingTime)
+	} else {
+		distance += (stats.speed * timeLeft)
+	}
+
+	return distance
+}
+
+func DayFourteenExample() {
+
+	fmt.Println("Day 14 - Example")
+
+	input := "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.\nDancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds."
+
+	reindeerStats := strings.Split(input, "\n")
+
+	maxDistance := 0
+	time := 1000
+	for i := 0; i < len(reindeerStats); i++ {
+		stats := parseReindeerStats(reindeerStats[i])
+		deerDistance := getDistanceForTime(stats, time)
+
+		if deerDistance > maxDistance {
+			maxDistance = deerDistance
+		}
+	}
+
+	fmt.Println("Max Distance:",maxDistance)
+
+}
+
+func DayFourteenPartOne() {
+
+	fmt.Println("Day 14 - Part One")
+
+	input := ReadFile("day14-input.txt")
+
+	reindeerStats := strings.Split(input, "\n")
+
+	maxDistance := 0
+	time := 2503
+
+	for i := 0; i < len(reindeerStats); i++ {
+		stats := parseReindeerStats(reindeerStats[i])
+		deerDistance := getDistanceForTime(stats, time)
+
+		if deerDistance > maxDistance {
+			maxDistance = deerDistance
+		}
+	}
+
+	fmt.Println("Max Distance:",maxDistance)
+
+}
