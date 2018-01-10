9fda74e271596d5e6f54fa05b76c9f894892ecbe
David Hodgson <dahodgson@gmail.com>
2018-01-10 23:30:45 +0000

    Day 20 - Part Two - Working down - slow!

--- DayTwenty.go
+++ DayTwenty.go
@@ -37,6 +37,20 @@ func getHouseScore(houseNumber int) int {
 	return total
 }
 
+
+func getLimitedHouseScore(houseNumber int) int {
+
+	factors := getFactors(houseNumber)
+	total := 0
+
+	for i := 0; i < len(factors); i++ {
+		if factors[i] * 50 <= houseNumber {
+			total += 11 * factors[i]
+		}
+	}
+	return total
+}
+
 func DayTwentyPartOne() {
 
 	fmt.Println("Day 20 - Part One")
@@ -55,4 +69,24 @@ func DayTwentyPartOne() {
 	}
 
 	fmt.Println("Lowest house number:", houseNumber)
+}
+
+func DayTwentyPartTwo() {
+
+	fmt.Println("Day 20 - Part Two")
+	input := 29000000
+
+	houseNumber := 0
+
+	for i := 7187040 ;i>0 ;i-- {
+
+		score := getLimitedHouseScore(i)
+		if score >= input {
+			houseNumber = i
+			fmt.Println("House",i,"is less than",input,"actually",score)
+		}
+	}
+
+	fmt.Println("House number", houseNumber)
+
 }
