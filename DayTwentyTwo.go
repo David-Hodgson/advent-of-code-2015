cadd9f7ee7716cce563d2883a6a3f392e19d84b0
David Hodgson <dahodgson@gmail.com>
2018-01-15 22:37:04 +0000

    Day 22 - More fleshing out

--- DayTwentyTwo.go
+++ DayTwentyTwo.go
@@ -5,6 +5,7 @@ import (
 )
 
 type spell struct {
+	name	 string
 	cost     int
 	damage   int
 	healing  int
@@ -17,20 +18,80 @@ type magicCharacter struct {
 	hitPoints int
 	mana      int
 	damage    int
+	canDoMagic bool
 }
 
-func doTurn(player, boss magicCharacter, effects []spell, availableSpells []spell) {
+func doTurn(attacker,defender magicCharacter, attackerEffects,defenderEffects map[string]spell, availableSpells []spell,manaSpent int) {
 
-	//appy effects
-	for i := 0; i < len(effects); i++ {
-		effectiveSpell := effects[i]
+	if attacker.hitPoints <= 0 || defender.hitPoints <= 0 {
+		fmt.Println("Someone is dead. Mana Spent:", manaSpent)
+		return
+	}
 
-		boss.hitPoints -= effectiveSpell.damage
-		player.hitPoints += effectiveSpell.healing
-		player.mana += effectiveSpell.recharge
+	//DO attack stuff
+	for name, attackerSpell := range attackerEffects {
+		fmt.Println("Processing attacker spell:", name,"-",attackerSpell, "duration:", attackerSpell.duration)
 
-		effectiveSpell.duration -= 1
+		if attackerSpell.duration > 0 {
+			attackerSpell.duration -= 1
+			spell := attackerEffects[name]
+			spell.duration -= 1
+			attackerEffects[name] = spell
 
+			attacker.hitPoints += spell.healing
+			attacker.mana += spell.recharge
+			defender.hitPoints -= spell.damage
+
+			if defender.hitPoints <= 0 {
+				fmt.Println("Some one killed by magic")
+				return
+			}
+		} else {
+			delete(attackerEffects, name)
+		}
+	}
+
+	for name, defenderSpell := range defenderEffects {
+		fmt.Println("Processing defender spell:", name,"-",defenderSpell, "duration:", defenderSpell.duration)
+
+		if defenderSpell.duration > 0 {
+			defenderSpell.duration -= 1
+
+			spell := defenderEffects[name]
+			spell.duration -= 1
+			attackerEffects[name] = spell
+
+			defender.hitPoints += spell.healing
+			defender.mana += spell.recharge
+			attacker.hitPoints -= spell.damage
+		} else {
+			delete(defenderEffects,name)
+		}
+	}
+	if attacker.canDoMagic {
+		fmt.Println("Attacker can do magic")
+
+		for i:=0; i< len(availableSpells); i++ {
+			spellToTry := availableSpells[i]
+			if _,ok := attackerEffects[spellToTry.name]; !ok && attacker.mana > spellToTry.cost {
+				fmt.Println("Casting spell", spellToTry.name)
+				attackerEffects[spellToTry.name] = spellToTry
+
+				doTurn(defender,attacker,defenderEffects,attackerEffects,availableSpells,manaSpent+spellToTry.cost)
+			}
+		}
+	} else {
+		fmt.Println("Attacker can't do magic")
+		attackerDamage := attacker.damage
+
+		defender.hitPoints -= attackerDamage
+		fmt.Println("Defender hitPoints:", defender.hitPoints)
+		if defender.hitPoints <= 0 {
+			fmt.Println("Non magic attack killed")
+			//return
+		}
+
+		doTurn(defender,attacker,defenderEffects,attackerEffects,availableSpells,manaSpent)
 	}
 }
 
@@ -38,30 +99,24 @@ func DayTwentyTwoExample() {
 
 	fmt.Println("Day 22 - Example")
 
-	magicMissile := spell{cost: 53, damage: 4, duration: 1}
-	drain := spell{cost: 73, damage: 2, healing: 2, duration: 1}
-	shield := spell{cost: 113, duration: 6, armour: 7}
-	poison := spell{cost: 172, duration: 6, damage: 3}
-	recharge := spell{cost: 229, duration: 5, recharge: 101}
+	magicMissile := spell{name: "Magic Missile", cost: 53, damage: 4, duration: 0}
+	drain := spell{name: "Drain", cost: 73, damage: 2, healing: 2, duration: 0}
+	shield := spell{name: "Shield", cost: 113, duration: 6, armour: 7}
+	poison := spell{name: "Poison", cost: 172, duration: 6, damage: 3}
+	recharge := spell{name: "Recharge", cost: 229, duration: 5, recharge: 101}
 
 	spells := []spell{magicMissile, drain, shield, poison, recharge}
 
 	fmt.Println(spells)
 
-	player := magicCharacter{10, 25, 0}
-	boss := magicCharacter{13, 0, 8}
+	player := magicCharacter{10, 250, 0,true}
+	boss := magicCharacter{13, 0, 8,false}
 
 	fmt.Println(player)
 	fmt.Println(boss)
 
-	for i := 0; i < 2; i++ {
+	playerEffects := make(map[string]spell)
+	bossEffects := make(map[string]spell)
 
-		//effects
-
-		//player turn
-
-		//effects
-
-		//boss turn
-	}
+	doTurn(player,boss,playerEffects,bossEffects,spells,0)
 }
